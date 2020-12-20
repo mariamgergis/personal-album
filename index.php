d1f84cad1b2e1ea98dfe0e4fe0ab7f44e15e3e25
mariamgergis <75936364+mariamgergis@users.noreply.github.com>
2020-12-21 01:47:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+<style>
+* { 
+    background-image: url('https://wallpapercave.com/wp/wp2636532.gif');
+    background-blend-mode: overlay;
+    background-repeat: repeat;
+    background-attachment: scroll;
+    box-sizing: border-box;
+
+}
+h1 {
+    width: 80%;
+    margin: 0px 0px 50px 140px;
+    padding: 30px 10px 30px 10px;
+    border: solid 3px #ffffff;
+    background: rgba(50, 164, 168, 0.3);
+    font-family: tahoma;
+    color: #ffffff;
+    letter-spacing: 3px;
+    text-align: center;
+}
+
+h1:hover {
+    background: #ffffff;
+    color: #000000;
+    opacity: 0.7;
+}
+
+h2 {
+    font-size: 13px;
+    font-family: georgia;
+    font-style: italic;
+    text-align: center;
+}
+
+p {
+    margin: 0px 0px 0px 0px;
+    padding: 40px 40px 40px 40px;
+    width: 100%;
+    text-align: center;
+    background: #ffffff;
+    border: solid #000000;
+    font-family: verdana;
+    font-size: 20px;
+    font-style: bold;
+    color: #000000;
+    opacity: 0.8;
+}
+p:hover {
+    background: rgba(250, 135, 223);
+    opacity: 0.7;
+}
+
+.p2 {
+    padding: 40px 40px 40px 40px;
+    width: 100%;
+    text-align: center;
+    background: #ffffff;
+    border: solid #000000;
+    font-family: verdana;
+    font-size: 20px;
+    font-style: bold;
+    color: #000000;
+    opacity: 0.8;
+}
+
+.p2:hover {
+    background: rgba(206, 135, 250);
+    opacity: 0.7;
+}
+
+.p3 {
+    padding: 40px 40px 40px 40px;
+    width: 100%;
+    text-align: center;
+    background: #ffffff;
+    border: solid #000000;
+    font-family: verdana;
+    font-size: 20px;
+    font-style: bold;
+    color: #000000;
+    opacity: 0.8;
+}
+
+.p3:hover {
+    background: rgba(135, 171, 250);
+    opacity: 0.7;
+}
+
+.p4 {
+    padding: 40px 40px 40px 40px;
+    width: 100%;
+    text-align: center;
+    background: #ffffff;
+    border: solid #000000;
+    font-family: verdana;
+    font-size: 20px;
+    font-style: bold;
+    color: #000000;
+    opacity: 0.8;
+}
+
+.p4:hover {
+    background: rgba(135, 242, 250);
+    opacity: 0.7;
+}
+
+.outer-grid {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 0px 0px 100px;
+    padding: 0 0px;
+}
+.inner-grid {
+    flex: 25%;
+    max-width: 23%;
+    padding: 0 0px;
+}
+.inner-grid img {
+    margin-top: 8px;
+    width: 100%;
+    padding: 10px;
+}
+@media screen and (max-width: 800px) {
+    .inner-grid {
+        flex: 50%;
+        max-width: 50%;
+    }
+}
+@media screen and (max-width: 600px) {
+   .inner-grid {
+       flex: 100%;
+       max-width: 100%;
+   } 
+}
+
+</style>
+<body>
+    <h1>Mariam's Personal Photo Album</h1>
+<div class="outer-grid">
+<div class="inner-grid"><p><b>ELEMENTARY DAYS</b>
+<a href="hoami.html"><img src="https://i.imgur.com/xHYP9uD.png"/></a>
+<a href="hoami.html"><img src="https://i.imgur.com/58pwDm2.jpg"/></a>
+<a href="hoami.html"><img src="https://i.imgur.com/wUt0Qde.jpg"/></a>
+<a href="hoami.html"><img src="https://i.imgur.com/VtD2uyY.png"/></a>
+<a href="hoami.html"><img src="https://i.imgur.com/1Ls1PJ4.jpg"/></a>
+</p>
+</div>   
+<div class="inner-grid">
+    <div class="p2"><b>MY HIGHSCHOOL DAYS</b>
+<a href="hoami1.html"><img src="https://i.imgur.com/eQSRgs1.jpg"/></a>
+<a href="hoami1.html"><img src="https://i.imgur.com/H8FH3Nr.jpg"/></a>
+<a href="hoami1.html"><img src="https://i.imgur.com/Ie60eGu.jpg"/></a>
+<a href="hoami1.html"><img src="https://i.imgur.com/CzZOC9J.jpg"/></a>
+<a href="hoami1.html"><img src="https://i.imgur.com/nS75hKA.jpg"/></a>
+</div>
+</div>   
+<div class="inner-grid">
+    <div class="p3"><b>MY SENIOR HIGHSCHOOL DAYS</b>
+<a href="hoami2.html"><img src="https://i.imgur.com/qNSUGcN.jpg"/></a>
+<a href="hoami2.html"><img src="https://i.imgur.com/2oqd7Jo.jpg"/></a>
+<a href="hoami2.html"><img src="https://i.imgur.com/eeDAVOg.jpg"/></a>
+<a href="hoami2.html"><img src="https://i.imgur.com/cBXBjQt.jpg"/></a>
+<a href="hoami2.html"><img src="https://i.imgur.com/jtnQguB.jpg"/></a>
+</div>
+</div>   
+<div class="inner-grid">
+    <div class="p4"><b>MY COLLEGE DAYS</b>
+<a href="hoami3.html"><img src="https://i.imgur.com/5OENynG.jpg"/></a>
+<a href="hoami3.html"><img src="https://i.imgur.com/mSWDCYu.jpg"/></a>
+<a href="hoami3.html"><img src="https://i.imgur.com/rGflFgx.jpg"/></a>
+<a href="hoami3.html"><img src="https://i.imgur.com/x4CKdWf.jpg"/></a>
+<a href="hoami3.html"><img src="https://i.imgur.com/GD4SWj6.jpg"/></a>
+</div>
+</div>
+</div>
+</body>
+</html>
